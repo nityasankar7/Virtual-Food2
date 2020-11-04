ce66833520b6e1173cf40ade5c32fb1bdc29cb1b
nityasankar7 <70414577+nityasankar7@users.noreply.github.com>
2020-11-04 18:42:31 -0500

    Update Food.js

--- Food.js
+++ Food.js
@@ -1,34 +1,25 @@
 class Food1{
 constructor(){
-  var foodStock,lastFed;
- // this.body = Bodies.rectangle(x, y,width,height);
- // World.add(world, this.body);
-  this.image = loadImage("Milk.png")
+   this.foodStock=0;
+     this.lastFed; 
+     this.image=loadImage("Milk.png");
+     
 }
-getFoodStock(){
-  var getFoodStock= database.ref('foodStock')
-    getFoodStock.on("value",function(data){
-     foodStock= data.val();
-  })
-}
+updateFoodStock(foodStock){
+   this.foodStock=foodStock; 
+  } 
+ getFedTime(lastFed){ 
+this.lastFed=lastFed;
+  } deductFood(){ 
+ if(this.foodStock>0){ this.foodStock=this.foodStock-1; } }
+ getFoodStock(){ 
+  return this.foodStock; }
 
- updateFoodStock(foodStock){
-  database.ref('/').update({
-  foodStock: Food
-  });
-}
-
-deductFood(lastFed){
-  var lastFed = "Food" + foodStock;
-  database.ref(lastFed).set({
-      Food: Food
-  })
-}
 display() {
   var x=80,y=100; 
     
     imageMode(CENTER);
-    image(this.image, 400,220,70,70);
+    image(this.image, 100,320,70,70);
     if(this.foodStock!=0){
     for(i=0;i<this.foodStock;i++){
       if(i%10==0){
